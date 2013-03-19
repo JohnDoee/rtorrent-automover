e9c4e2d51cdba2bc912d64176db63d48f591dd39
JohnDoee <supersecretemail@example.com>
2013-03-19 16:46:07 +0100

    Free space buffer was added the wrong way

--- automover/section.py
+++ automover/section.py
@@ -35,7 +35,7 @@ class Automover(object):
             name = torrent.name
             size = torrent.get_size()
 
-            destination_paths = [p for p in self.destination_paths if self.get_free_space(p) + 10*1024*1024 > size]
+            destination_paths = [p for p in self.destination_paths if self.get_free_space(p) - 10*1024*1024 > size]
             if not destination_paths:
                 self.logger.debug('No free space for %s (need %s)' % (torrent, size))
                 continue
