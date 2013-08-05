b20e9a8e2a415f414f0f93174c33273e464123fe
JohnDoee <supersecretemail@example.com>
2013-08-05 09:53:09 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='rtorrent-automover',
-    version='2.2.0',
+    version='2.2.1',
     description='Automover ', 
     author='John Doee',
     maintainer='John Doee',
